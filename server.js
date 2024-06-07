90bf0a7a38c8e0422e10d178fa9dc8b215673c1f
charlesC137 <139760747+charlesC137@users.noreply.github.com>
2024-06-07 21:26:27 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const logInRouter = require("./routes/log-in");
 const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: false }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
 app.use(signUpRouter);
 app.use(logInRouter);
 
