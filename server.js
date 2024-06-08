459d129ddcb5dc8a26c53d8619987db76085cb86
charlesC137 <139760747+charlesC137@users.noreply.github.com>
2024-06-08 17:56:54 +0100

    Update server.js --

--- server.js
+++ server.js
@@ -12,23 +12,23 @@ const updateCartRouter = require("./routes/update-cart")
 
 const port = process.env.PORT || 3000;
 
- app.use(
-   "/api",
-   createProxyMiddleware({
-     target: "https://api-selham.onrender.com",
-     changeOrigin: true,
-     pathRewrite: {
-       "^/api": "/api",
-     },
-     onProxyRes: (proxyRes) => {
-       proxyRes.headers["Access-Control-Allow-Origin"] = "*";
-       proxyRes.headers["Access-Control-Allow-Methods"] =
-         "GET, POST, PUT, DELETE, OPTIONS";
-       proxyRes.headers["Access-Control-Allow-Headers"] =
-         "Origin, X-Requested-With, Content-Type, Accept, Authorization";
-     },
-   })
- );
+app.use(
+  "/api",
+  createProxyMiddleware({
+    target: "https://api-selham.onrender.com",
+    changeOrigin: true,
+    pathRewrite: {
+      "^/api": "/api",
+    },
+    onProxyRes: (proxyRes) => {
+      proxyRes.headers["Access-Control-Allow-Origin"] = "*";
+      proxyRes.headers["Access-Control-Allow-Methods"] =
+        "GET, POST, PUT, DELETE, OPTIONS";
+      proxyRes.headers["Access-Control-Allow-Headers"] =
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization";
+    },
+  })
+);
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
